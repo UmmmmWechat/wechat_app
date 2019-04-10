e85e29840ba4e0d8f71c1e6db71c5b15ceb8a2fc
161250013 <161250013@smail.nju.edu.cn>
2019-04-10 21:43:01 +0800

    remove server

--- tourpal_server/src/main/java/ummm/tourpal/entity/ResultMessage.java
+++
@@ -1,9 +0,0 @@
-package ummm.tourpal.entity;
-
-public enum ResultMessage {
-    OK,
-    ALREADY_CANCELED,
-    ALREADY_TIMEOUT,
-    ALREADY_ACCEPTED,
-    ALREADY_REJECTED
-}
--- tourpal_server/src/main/java/ummm/tourpal/util/APIUtil.java
+++
@@ -1,7 +0,0 @@
-package ummm.tourpal.util;
-
-public class APIUtil {
-    public static String getOpenId(String code) {
-        return null;
-    }
-}
